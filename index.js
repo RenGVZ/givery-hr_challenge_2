2b79f1253fd4f3b0efb7d812c6ff8919497eba34
Gavin Garza <gaving@live.com>
2023-08-02 23:27:46 +0900

    deploy modification

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = require('./app')
 const config = require('./utils/config')
 
-const PORT = config.PORT || 3001
+const PORT = process.env.PORT || 3001
+
+console.log('process.env:', process.env)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
